1e45bef20c34416b2e4d0f374366afd84172abeb
ShadyMoon <103329255+ShadyM00n@users.noreply.github.com>
2025-02-20 23:22:03 -0500

    Update index.jsx

--- vrt0/src/pages/index.jsx
+++ vrt0/src/pages/index.jsx
@@ -1,6 +1,8 @@
 const Index = () => {
   return (
-    <div>Index</div>
+    <div className="bg-zinc-950 w-screen h-screen text-zinc-100 items-center justify-center flex">
+      <a href="https://github.com/ShadyM00n/ShadyJS-CLI" target="_blank" className="text-center font-bold text-3xl">Vite React Tailwind Template</a>
+    </div>
   )
 }
 
